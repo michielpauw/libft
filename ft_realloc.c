2ec328d971aaf7eabc09f70af045309440c40d9d
Michiel Pauw <pauw.michiel@gmail.com>
2018-02-15 11:48:50 +0100

    bleh

--- ft_realloc.c
+++ ft_realloc.c
@@ -6,7 +6,8 @@ void	ft_realloc(void **s, size_t old_size, size_t new_size)
 	unsigned char	*m;
 
 	m = *(unsigned char **)s;
-	if (!(*(unsigned char **)s = (unsigned char *)malloc(sizeof(unsigned char) * new_size)))
+	if (!(*(unsigned char **)s =
+				(unsigned char *)malloc(sizeof(unsigned char) * new_size)))
 		return ;
 	i = 0;
 	while (i < old_size)
@@ -16,23 +17,3 @@ void	ft_realloc(void **s, size_t old_size, size_t new_size)
 	}
 	free(m);
 }
-
-/*
-void	ft_realloc(char *str, size_t *prev_mem)
-{
-	char	*temp;
-	size_t	i;
-
-	i = 0;
-	temp = str;
-	if (!(str = (char *)malloc(sizeof(char) * (*prev_mem * 2 + 1))))
-		return ;
-	while (*(temp + i))
-	{
-		*(str + i) = *(temp + i);
-		i++;
-	}
-	*prev_mem *= 2;
-	free(temp);
-}
-*/
