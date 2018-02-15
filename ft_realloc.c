7f84b3c6c014752bd83f454026ab28ddfce244e0
Michiel Pauw <pauw.michiel@gmail.com>
2018-02-15 11:47:52 +0100

    Pfff...

--- ft_realloc.c
+++ ft_realloc.c
@@ -1,12 +1,12 @@
 #include "libft.h"
 
-void	ft_realloc(void *s, size_t old_size, size_t new_size)
+void	ft_realloc(void **s, size_t old_size, size_t new_size)
 {
 	size_t			i;
 	unsigned char	*m;
 
 	m = *(unsigned char **)s;
-	if (!(*s = (unsigned char *)malloc(sizeof(unsigned char) * new_size)))
+	if (!(*(unsigned char **)s = (unsigned char *)malloc(sizeof(unsigned char) * new_size)))
 		return ;
 	i = 0;
 	while (i < old_size)
@@ -16,3 +16,23 @@ void	ft_realloc(void *s, size_t old_size, size_t new_size)
 	}
 	free(m);
 }
+
+/*
+void	ft_realloc(char *str, size_t *prev_mem)
+{
+	char	*temp;
+	size_t	i;
+
+	i = 0;
+	temp = str;
+	if (!(str = (char *)malloc(sizeof(char) * (*prev_mem * 2 + 1))))
+		return ;
+	while (*(temp + i))
+	{
+		*(str + i) = *(temp + i);
+		i++;
+	}
+	*prev_mem *= 2;
+	free(temp);
+}
+*/
