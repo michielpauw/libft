2195fe2e6059fe35501866813664b7ec4e379f65
Michiel Pauw <pauw.michiel@gmail.com>
2018-02-15 11:56:42 +0100

    All is well again

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mpauw <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/11/14 08:40:27 by mpauw             #+#    #+#             */
+/*   Updated: 2017/11/27 16:44:11 by mpauw            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# define BUFF_SIZE 32 
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include "libft/libft.h"
+
+int					get_next_line(const int fd, char **line);
+
+typedef struct		s_buff
+{
+	int				fd;
+	char			*buff;
+}					t_buff;
+
+#endif
